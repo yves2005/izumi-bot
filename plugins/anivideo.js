d357de01f01063c4d4349d0fa250d1a3d9708f74
Eypz <105191630+sataniceypz@users.noreply.github.com>
2025-02-03 16:22:15 +0530

    type

--- plugins/anivideo.js
+++ plugins/anivideo.js
@@ -4,7 +4,7 @@ izumi ({
     pattern: "naruto",
     fromMe: mode,
     desc: "random Naruto anime videos",
-    type: "AnimeVideo",
+    type: "Anime",
 }, async (message, match) => {
   const { result } = await getJson(apiUrl + 'api/anime/naruto?apikey=izumi-v3');
   message.sendFromUrl(result.url, {
@@ -26,30 +26,9 @@ izumi ({
     pattern: "anime",
     fromMe: mode,
     desc: "random  anime videos",
-    type: "AnimeVideo",
+    type: "Anime",
 }, async (message, match) => {
 const { result } = await getJson(apiUrl + 'api/anime/anivideo?apikey=izumi-v3');
-  message.sendFromUrl(result.url, {
-    contextInfo: {
-      mentionedJid: [message.sender],
-      forwardingScore: 1,
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363298577467093@newsletter',
-        newsletterName: "Iᴢᴜᴍɪ-ᴠ3",
-        serverMessageId: -1
-      }
-    },
-    caption: `${config.CAPTION}`
-  });
-});
- izumi ({
-    pattern: "mstatus",
-    fromMe: mode,
-    desc: "random Malayalam status videos",
-    type: "media",
-}, async (message, match) => {
-const { result } = await getJson(apiUrl + 'api/randomvideo/msts?apikey=izumi-v3')
   message.sendFromUrl(result.url, {
     contextInfo: {
       mentionedJid: [message.sender],
@@ -68,7 +47,7 @@ izumi ({
     pattern: "onepiece",
     fromMe: mode,
     desc: "random one-piece anime status ",
-    type: "AnimeVideo",
+    type: "Anime",
 }, async (message, match) => {
 const url = apiUrl + 'api/anime/onepiece?apikey=izumi-v3'
   message.sendFromUrl(url, {
@@ -89,7 +68,7 @@ izumi ({
     pattern: "demonslayer",
     fromMe: mode,
     desc: "random demonslayer anime status ",
-    type: "AnimeVideo",
+    type: "Anime",
 }, async (message, match) => {
 const url = apiUrl + 'api/anime/demonslayer?apikey=izumi-v3'
   message.sendFromUrl(url, {
