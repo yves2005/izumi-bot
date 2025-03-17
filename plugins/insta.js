fd17dd480c4e6d0fd8ffee58793857f56eb998dc
Prince Rudh <87886877+princerudh@users.noreply.github.com>
2025-03-18 02:31:31 +0530

    Update insta , fb

--- plugins/insta.js
+++ plugins/insta.js
@@ -31,3 +31,41 @@ izumi({
         console.error(error);
     }
 });
+
+izumi({
+    pattern: 'fb ?(.*)',
+    fromMe: mode,
+    desc: 'Download facebook videos.',
+    type: 'downloader',
+}, async (message, match, client) => {
+    try {
+        const url = match || message.reply_message.text;
+        if (!url) {
+            return await message.reply("Please provide a valid Instagram URL.");
+        }
+
+        const fbApi = `https://api.siputzx.my.id/api/d/igdl?url=${url}`;
+        const res = await fetch(fbApi);
+        if (!res.ok) {
+            return await message.reply("Please try again.");
+        }
+        
+        const data = await res.json();
+        const igmedia = data.data;
+
+        if (igmedia && igmedia.length > 0) {
+            let counter = 0;
+            for (const media of igmedia) {
+                if (counter >= 10) break;
+                const mediaurl = media.url;
+                await message.sendFile(mediaurl);
+                counter++;
+            }
+        } else {
+            await message.reply("No media found for the provided URL.");
+        }
+    } catch (error) {
+        console.error(error);
+        await message.reply(" 'error' ");
+    }
+});
