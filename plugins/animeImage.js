ad4a8d41ab5ad0f8e3464ed4f51ac77d69848b4d
Eypz <105191630+Akshay-Eypz@users.noreply.github.com>
2025-03-28 01:12:35 +0530

    Update animeImage.js

--- plugins/animeImage.js
+++ plugins/animeImage.js
@@ -1,6 +1,5 @@
 const { izumi, mode, getJson } = require("../lib");
 const config = require("../config");
-const { getApiConfig } = require('../lib/api.js');
 izumi({
   pattern: "waifu",
   fromMe: mode,
@@ -9,14 +8,6 @@ izumi({
 }, async (message, match) => {
   var { url } = await getJson('https://api.waifu.pics/sfw/waifu');
   await message.sendFromUrl(url,{caption: `${config.CAPTION}`});
-});
-izumi({
-  pattern: "tsunade",
-  fromMe: mode,
-  desc: "Random anime images",
-  type: "Anime",
-}, async (message, match) => {
-  await message.sendFromUrl(eypzApi + 'tsunade',{caption: `${config.CAPTION}`});
 });
 izumi({
   pattern: "neko",
@@ -35,14 +26,4 @@ izumi({
 }, async (message, match) => {
   var { url } = await getJson('https://api.waifu.pics/sfw/neko');
   await message.sendFromUrl(url,{caption: `${config.CAPTION}`});
-});
-
-
-izumi({
-  pattern: "shinobu",
-  fromMe: true,
-  desc: "Random anime images",
-  type: "Anime",
-}, async (message, match) => {
-  await message.sendFromUrl(apiUrl + 'api/anime/loli?apikey=izumi-v3',{caption: `${config.CAPTION}`});
 });
